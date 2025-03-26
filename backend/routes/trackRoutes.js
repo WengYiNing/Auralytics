eb08dd4da500dd05bf7f70e84f22754f5090167f
weng yi-ning <w20020112@gmail.com>
2025-03-26 18:38:17 +0800

    Update trackRoutes.js

--- backend/routes/trackRoutes.js
+++ backend/routes/trackRoutes.js
@@ -30,19 +30,6 @@ module.exports = function(client) {
         }
     };
 
-    const validateToken = async (accessToken) => {
-        try {
-            const response = await axios.get('https://api.spotify.com/v1/me', {
-                headers: { 'Authorization': `Bearer ${accessToken}` }
-            });
-            console.log("Token is valid. User data:", response.data);
-            return true;  
-        } catch (error) {
-            console.error("Token validation failed:", error.response ? error.response.data : error.message);
-            return false;  
-        }
-    };
-
     router.get('/top-tracks/:userId/:timeRange', async (req, res) => {
         const { userId, timeRange } = req.params;
         const authHeader = req.headers['authorization'];
