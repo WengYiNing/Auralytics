3b4b0f6c3a74a3b83c7d946296df0fe1f93eb92d
weng yi-ning <w20020112@gmail.com>
2025-04-09 16:49:54 +0800

    install the dependency of i18n

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,8 +1,7 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
-
-const initReactI18next = require('react-i18next/initReactI18next');
+import { initReactI18next } from 'react-i18next';
 
 i18n
   .use(HttpApi)
